aebffba0e5257d85cc501929f002064ad1973bec
Mike Pullen <mike.pullen@gmail.com>
2025-01-05 18:03:32 -0600

    very basic ui

---
+++ app.js
@@ -0,0 +1,15 @@
+function initializeGame() {
+  const cells = document.querySelectorAll(".cell");
+  console.log(cells);
+  cells.forEach((cell) => {
+    cell.addEventListener("click", handleCellClick);
+  });
+}
+
+function handleCellClick() {
+  this.innerText = currentPlayer;
+  currentPlayer = currentPlayer === "X" ? "O" : "X";
+}
+
+initializeGame();
+let currentPlayer = "X";
