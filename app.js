7dc72efbbca5f8856f0516bd41875bd9ef568aeb
Mike Pullen <mike.pullen@gmail.com>
2025-01-05 21:21:14 -0600

    create board and populate

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 // @ts-nocheck
+let currentPlayer = "X";
+let board = [
+  ["-", "-", "-"],
+  ["-", "-", "-"],
+  ["-", "-", "-"],
+];
+
 function initializeGame() {
   const cells = document.querySelectorAll(".cell");
   console.log(cells);
@@ -8,9 +15,46 @@ function initializeGame() {
 }
 
 function handleCellClick() {
-  this.innerText = currentPlayer;
-  currentPlayer = currentPlayer === "X" ? "O" : "X";
+  const clickedCell = this;
+  if (isPlayed(clickedCell)) return;
+  updateBoard(clickedCell);
+}
+function isPlayed(cell) {
+  if (cell.innerText === "X" || cell.innerText === "O") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function nextPlayer(player) {
+  return player === "X" ? "O" : "X";
+}
+
+function row(cell) {
+  return Math.floor(cell.dataset.index / 3);
+}
+
+function col(cell) {
+  return cell.dataset.index - row(cell) * 3;
+}
+
+function updateBoard(cell) {
+  board[row(cell)][col(cell)] = currentPlayer;
+  printBoard(board);
+  cell.innerText = currentPlayer;
+  currentPlayer = nextPlayer(currentPlayer);
+}
+
+function printBoard(board) {
+  for (let row = 0; row < 3; row++) {
+    let line = "";
+    for (let col = 0; col < 3; col++) {
+      line += board[row][col] + " ";
+    }
+    console.log(line);
+  }
+  console.log("\n");
 }
 
 initializeGame();
-let currentPlayer = "X";
